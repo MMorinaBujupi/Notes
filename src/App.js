50cbc9ea69a5907d13908124f4fa876afbf3bafc
MMorinaBujupi <marigona.morina25@gmail.com>
2022-09-14 13:37:13 +0200

    new

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import {nanoid} from 'nanoid';
+import NoteList  from './componentes/NotesList'
+import AddNote from './componentes/AddNote';
+import Search from './componentes/Search';
+import Header from './componentes/Header';
+function App() {
+  const [notes, setNotes] = useState([
+    {
+    id :nanoid(),
+    text :"This is my first  NOTE!",
+    date :"12/09/2022",
+    
+  },
+  {
+    id :nanoid(),
+    text :"This is  my second NOTE!",
+    date :"13/09/2022",
+    
+  },{
+    id :nanoid(),
+    text :"This is my  third  NOTE!",
+    date: "1/09/2022",
+    
+  },
+]);
+const [searchText, setsearchText] =useState('');
+const [darkMode, setDarkMode] = useState(false);
+
+
+useEffect(() => {
+  const savedNotes = JSON.parse(
+    localStorage.getItem('react-my-app1-data')
+  );
+
+  if (savedNotes) {
+    setNotes(savedNotes);
+  }
+}, []);
+
+useEffect(() => {
+  localStorage.setItem(
+    'react-my-app1-data',
+    JSON.stringify(notes)
+  );
+}, [notes]);
+
+const addNote = (text) => {
+  // console.log(text);
+  const date = new Date ();
+  const newNote = {
+    id :nanoid(),
+    text :text,
+    date :date.toLocaleDateString(),
+  }
+  const newNotes = [...notes, newNote];
+		setNotes(newNotes);
+}
+const deleteNote = (id) => {
+  const newNotes = notes.filter((note) => note.id !== id);
+  setNotes(newNotes);
+};
+
+   return (
+    <div className={`${darkMode && 'dark-mode'}`}>
+    <div className="contaniner">
+      <Header handleToggleDarkMode={setDarkMode} />
+    {/* <h2>Note</h2> */}
+    <Search handelSearchNote = { setsearchText}/> 
+      <NoteList
+       notes ={notes.filter((note) =>
+         note.text.toLowerCase().includes(searchText))}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+        />
+      
+     
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
